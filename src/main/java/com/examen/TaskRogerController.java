d9b5ad109a069f1b824111a3138b781f7c5c7ce2
Roger963 <roger.pinedapaz@gmail.com>
2024-05-22 01:16:50 -0500

    getall

--- src/main/java/com/examen/TaskRogerController.java
+++ src/main/java/com/examen/TaskRogerController.java
@@ -1,15 +1,14 @@
 package com.examen;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Servlet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.net.URI;
+
 
 @RestController
 @RequestMapping("/api/v1/roger")
@@ -18,6 +17,11 @@ public class TaskRogerController {
     private TaskRogerService taskRogerService;
 
 
+    @GetMapping("/all")
+    public ResponseEntity<Page<TaskRogerDTO>> findAll(Pageable pageable) {
+        Page<TaskRogerDTO> list = taskRogerService.findAllPaged(pageable);
+        return ResponseEntity.ok().body(list);
+    }
 
     @PostMapping
     public ResponseEntity<TaskRogerDTO> insert(@RequestBody TaskRogerDTO taskRogerDTO) {
